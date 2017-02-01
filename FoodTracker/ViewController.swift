3f2a2af88a0b27b66f2f30b0dc7dd8124f326ec8
Igor-Senshov <Noct@Noctiss-Mac.local>
2017-02-01 10:12:04 -0500

    Outlets and Basic Action

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: Properties.
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var mealNameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +24,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: Actions.
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+    
 
 
 }
