028da3d635804a6822fa49cdebabe3e1d25aa245
Igor-Senshov <Noct@Noctiss-Mac.local>
2017-02-07 09:52:41 -0500

    Added RatingControl UIStackView Subclass.

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,22 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Noctis Lucis Caelum on 2/7/17.
+//  Copyright © 2017 Noctis Lucis Caelum. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    //MARK: Initialization.
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+}
