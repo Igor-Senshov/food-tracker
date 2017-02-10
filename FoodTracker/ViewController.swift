6f0fc89364cd8d8e0ff1200c451af3266883a43e
Igor Senshov <Igor-Senshov@users.noreply.github.com>
2017-02-10 10:16:20 -0500

    Connected Rating Control to the View Controller. Cleanup and centering.

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
     
 
     override func viewDidLoad() {
@@ -75,11 +77,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         imagePickerController.delegate = self
         present(imagePickerController, animated: true, completion: nil)
     }
-    
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-    }
-    
 
 
 }
